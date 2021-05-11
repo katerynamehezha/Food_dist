3a06fdaf85ab4516be7ace8fbb9534aeb00b8a7d
kateryna <katerynamehezha@gmail.com>
2021-05-11 11:56:32 +0300

    timer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
+  // Tabs
   const tabs = document.querySelectorAll('.tabheader__item')
   const tabsContent = document.querySelectorAll('.tabcontent')
   const tabsParent = document.querySelector('.tabheader__items')
@@ -33,4 +34,61 @@ window.addEventListener('DOMContentLoaded', () => {
       })
     }
   })
+
+  // Timer
+
+  const deadline = '2021-05-20'
+
+  function getTimerRemaining(endtime) {
+    // определяет разницу между дедлайном и нашим текущим аременем
+    const t = Date.parse(endtime) - Date.parse(new Date()) // разница между датами в количистве милисекунд
+    const days = Math.floor(t / (1000 * 60 * 60 * 24)) // считаем количество дней
+    const hours = Math.floor((t / (1000 * 60 * 60)) % 24)
+    const minutes = Math.floor((t / 1000 / 60) % 60)
+    const seconds = Math.floor((t / 1000) % 60)
+
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    }
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`
+    } else {
+      return num
+    }
+  }
+
+  function setClock(selector, endtime) {
+    // установим часы
+    const timer = document.querySelector(selector)
+    const days = timer.querySelector('#days')
+    const hours = timer.querySelector('#hours')
+    const minutes = timer.querySelector('#minutes')
+    const seconds = timer.querySelector('#seconds')
+    const timeInterval = setInterval(updateClock, 1000)
+
+    updateClock()
+
+    function updateClock() {
+      // обновлять таймер каждую секунду
+      const t = getTimerRemaining(endtime)
+
+      days.innerHTML = getZero(t.days)
+      hours.innerHTML = getZero(t.hours)
+      minutes.innerHTML = getZero(t.minutes)
+      seconds.innerHTML = getZero(t.seconds)
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval)
+      }
+    }
+  }
+
+  setClock('.timer', deadline)
 })
